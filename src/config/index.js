456a0fd58c979b4927bf54bcf6f85a4a02e59321
Dch <daichenhui@huishoubao.com.cn>
2019-08-09 16:59:25 +0800

    update apiUrl
    update apiUrl

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
     IS_DEV: process.env.NODE_ENV === "development",
-    hostUrl: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8889' : ''
+    hostUrl: process.env.NODE_ENV === "development" ? 'http://127.0.0.1:8889' : 'http://lemonapi.mclemon1.com'
 }
